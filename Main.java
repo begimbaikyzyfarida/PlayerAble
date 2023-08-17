b4a4c246b081c09691adf4f4d1289d97cd49bb63
FARIDA <140826748+begimbaikyzyfarida@users.noreply.github.com>
2023-08-17 21:15:45 +0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,17 +1,30 @@
 import java.time.LocalDate;
 public class Main {
     public static void main(String[] args) {
-        PlayerAble[] team = {
-                new Forwards("Azamat", "Begimbai uulu", LocalDate.of(2004, 12, 22), 185, 80),
-                new Defenders("Farida", "Begimbai kyzy", LocalDate.of(2003, 7, 17), 157, 55),
-                new Goalkeeper("Bekzat", "Jusupmamatov", LocalDate.of(2013, 7, 7), 165, 60),
-                new Trainer("Begimbai", "Maksutov")};
 
-        for (PlayerAble player : team) {
-            System.out.println("---------------------------------");
-            player.printPlayerInfo();
-            System.out.println("Age:"+ player.getYearsComanda());
-            System.out.println("---------------------------------");
-        }
+        Forwards forward=new Forwards("Azamat","Begimbai uulu",LocalDate.of(2004,12,22),185,80,"Forwarder");
+        System.out.println(forward);
+        forward.printPlayerInfo();
+        forward.forwards();
+        forward.getYearsComanda();
+        System.out.println("-----------------------------------");
+
+        Defenders defender = new Defenders("Farida", "Begimbai kyzy", LocalDate.of(2003, 7, 17), 157, 55,"Defenders");
+        System.out.println(defender);
+        defender.printPlayerInfo();
+        defender.defenders();
+        defender.getYearsComanda();
+        System.out.println("-----------------------------------");
+        Goalkeeper goalkeepers= new Goalkeeper("Bekzat", "Jusupmamatov", LocalDate.of(2013, 7, 7), 165, 60,"Goalkeeper");
+        System.out.println(goalkeepers);
+        goalkeepers.printPlayerInfo();
+        goalkeepers.goalkeeper();
+        goalkeepers.getYearsComanda();
+        System.out.println("-----------------------------------");
+        Trainer trainer =  new Trainer("Begimbai", "Maksutov");
+        System.out.println(trainer);
+        trainer.getName();
+        trainer.getSurname();
+        trainer.printPlayerInfo();
     }
 }
